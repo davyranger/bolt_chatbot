5c27f113c138ab245fd23ab6bfa66af66c08ab6f
davy <davy.strain@hotmail.com>
2024-11-02 18:31:30 +0800

    modularising the code

--- variables.tf
+++ variables.tf
@@ -1,3 +1,12 @@
+variable "slack_bot_token" {
+  description = "Slack bot token"
+  type        = string
+}
+
+variable "slack_app_token" {
+  description = "Slack app token"
+  type        = string
+}
 
 
 
