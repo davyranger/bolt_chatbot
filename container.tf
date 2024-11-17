10cf0d6a7d793bfcd8fd017e61dc519df70ddf91
Davy <42505476+davyranger@users.noreply.github.com>
2024-11-17 12:10:08 +0800

    Multiple workflows v2 (#80)
    * first push on this branch
    
    * first push on this branch
    
    * import azuread_service_principal

--- container.tf
+++ container.tf
@@ -46,6 +46,11 @@ data "azuread_application" "existing_app" {
   display_name = "github-actions-terraform-authenticate" # Replace with your existing app registration ID
 }
 
+import {
+  to = azuread_service_principal.sp
+  id = "71fdc874-cc03-4e4f-b597-2de49c07589f"
+}
+
 resource "azuread_service_principal" "sp" {
   client_id = data.azuread_application.existing_app.client_id
 }
