d38636be56686d5dc2cca4e4b912d6e9fa770f73
davy <davy.strain@hotmail.com>
2024-11-20 20:44:13 +0800

    added rg name to repo

--- container.tf
+++ container.tf
@@ -35,7 +35,7 @@ data "azurerm_resource_group" "example" {
 
 # Container Registry Data Source
 data "azurerm_container_registry" "example" {
-  name                = "boltslackbotcontainerregistry"
+  name                = "boltslackbotacr"
   resource_group_name = data.azurerm_resource_group.example.name
 }
 
--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_container_registry" "example" {
-  name                = "boltslackbotcontainerregistry"
+  name                = "boltslackbotacr"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Standard"
