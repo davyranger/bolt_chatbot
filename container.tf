cdfe1deffccde5447b8678382da8a07aca6c157b
davy <davy.strain@hotmail.com>
2024-11-24 09:19:13 +0800

    removed admin enabled

--- container.tf
+++ container.tf
@@ -52,7 +52,7 @@ resource "azurerm_user_assigned_identity" "managed_identity" {
 # Role Assignments
 resource "azurerm_role_assignment" "acr_pull" {
   principal_id         = azurerm_user_assigned_identity.managed_identity.principal_id
-  role_definition_name = "Owner"
+  role_definition_name = "AcrPull"
   scope                = data.azurerm_container_registry.example.id
 }
 
