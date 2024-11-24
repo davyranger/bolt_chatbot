59f162823df3ab88667a298f2a11331c69322504
Davy <42505476+davyranger@users.noreply.github.com>
2024-11-24 09:06:58 +0800

    Newest 24 (#131)
    * setup new repo
    
    * setup new repo
    
    * setup new repo
    
    * setup new repo
    
    * setup new repo
    
    * added roles to main.tf
    
    * added roles to main.tf
    
    * added roles to main.tf
    
    * added roles to main.tf
    
    * added roles to main.tf
    
    * removed admin enabled

--- main.tf
+++ main.tf
@@ -51,6 +51,5 @@ resource "azurerm_container_registry" "example" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Standard"
-  admin_enabled       = true
 }
 
