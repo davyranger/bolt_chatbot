d84ed1e4e2b130c6a251c104072da789e8276bb2
davy <davy.strain@hotmail.com>
2024-11-25 19:53:42 +0800

    added all files to one

--- main.tf
+++ main.tf
@@ -32,10 +32,28 @@ provider "azurerm" {
   resource_provider_registrations = "none" # Disable automatic resource provider registrations
 }
 
+data "azuread_service_principal" "sp" {
+  object_id = "fddda90e-aa3d-414c-97a3-b30a56ecbbf3"
+}
+
 # Define an Azure Resource Group for organizing resources
 resource "azurerm_resource_group" "rg" {
   name     = "slack-bot-rg"     # Name of the resource group
   location = "australiacentral" # Azure region where the resource group is located
+}
+
+# Managed Identity
+resource "azurerm_user_assigned_identity" "managed_identity" {
+  name                = "slackbot-identity"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Role Assignments
+resource "azurerm_role_assignment" "acr_pull" {
+  principal_id         = azurerm_user_assigned_identity.managed_identity.principal_id
+  role_definition_name = "AcrPull"
+  scope                = data.azurerm_container_registry.example.id
 }
 
 resource "azurerm_container_registry" "example" {
@@ -44,4 +62,41 @@ resource "azurerm_container_registry" "example" {
   location            = azurerm_resource_group.rg.location
   sku                 = "Standard"
 }
+
+# Container Group
+resource "azurerm_container_group" "example" {
+  name                = "boltslackbotgroup"
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
+  os_type             = "Linux"
+
+  identity {
+    type         = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.managed_identity.id]
+  }
+
+  container {
+    name   = "boltslackbot"
+    image  = "${data.azurerm_container_registry.example.login_server}/slack-bot:latest"
+    cpu    = "1.0"
+    memory = "1.5"
+
+    ports {
+      port = 80
+    }
+
+    environment_variables = {
+      SLACK_BOT_TOKEN = var.slack_bot_token
+      SLACK_APP_TOKEN = var.slack_app_token
+    }
+  }
+
+  image_registry_credential {
+    user_assigned_identity_id = azurerm_user_assigned_identity.managed_identity.id
+    # username                  = var.acr_username
+    # password                  = var.acr_password
+    server = data.azurerm_container_registry.example.login_server
+  }
+}
+
 
