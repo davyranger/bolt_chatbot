92b3157cc8eb66aa49615d74ac039f0f800b1e27
davy <davy.strain@hotmail.com>
2024-11-26 08:01:30 +0800

    first commit

--- main.tf
+++ main.tf
@@ -13,11 +13,11 @@ terraform {
   required_version = "~> 1.9.0"
 
   # Configuration for storing Terraform state remotely in an Azure storage account
-  
+
   backend "azurerm" {
-    resource_group_name  = "platform-terraform-state" # Resource group where the storage account is located
-    storage_account_name = "davyterraform"            # Azure Storage account for storing the state file
-    container_name       = "slackbot"             # Blob container where the state file will be stored
+    resource_group_name  = "terraform" # Resource group where the storage account is located
+    storage_account_name = "workflowstatefiles"            # Azure Storage account for storing the state file
+    container_name       = "slackbotstate"             # Blob container where the state file will be stored
     key                  = "terraform.tfstate"        # Name of the Terraform state file
     use_oidc             = true                       # Enable OIDC for authentication with Azure
   }
