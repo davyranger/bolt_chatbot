92fc70bc1b1debf3b474d8256afb16ff9562e632
davy <davy.strain@hotmail.com>
2024-12-01 17:06:50 +0800

    adding in variables to container group for testing

--- infra/parent_directory/container_group.tf
+++ infra/parent_directory/container_group.tf
@@ -4,17 +4,10 @@ module "container_group" {
   resource_group_name     = module.container.rg_name
   resource_group_location = module.container.rg_location
   # user_assigned_id        = module.container.uai_id
-<<<<<<< HEAD
   container_registry    = module.container.cg_id
   slack_bot_token       = var.slack_bot_token
   slack_app_token       = var.slack_app_token
   azure_subscription_id = var.azure_subscription_id
-=======
-  container_registry      = module.container.cg_id
-  slack_bot_token         = var.slack_bot_token
-  slack_app_token         = var.slack_app_token
-  azure_subscription_id   = var.azure_subscription_id
->>>>>>> 37d3ac421657c1cdfed38ff94dbbbb0451299037
 
   depends_on = [
     module.container
