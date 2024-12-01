702a5e5439c29bc2d560a9780d3e841fe9ee9116
davy <davy.strain@hotmail.com>
2024-12-01 08:44:52 +0800

    refactoring code

--- infra/modules/container/outputs.tf
+++ infra/modules/container/outputs.tf
@@ -1,11 +1,11 @@
 output "rg_id" {
-  value = data.azurerm_resource_group.rg.id
+  value = azurerm_resource_group.rg.id
 }
 
 output "uai_principal_id" {
-  value = data.azurerm_user_assigned_identity.managed_identity.principal_id
+  value = azurerm_user_assigned_identity.managed_identity.principal_id
 }
 
 output "cg_id" {
-  value = data.azurerm_container_registry.example.id
+  value = azurerm_container_registry.example.id
 }
