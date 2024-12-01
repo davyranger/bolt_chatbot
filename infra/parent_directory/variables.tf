0235366d99aafbb36b293c41ff81e2ef393e251e
davy <davy.strain@hotmail.com>
2024-12-01 10:30:52 +0800

    refactoring code

--- infra/parent_directory/variables.tf
+++ infra/parent_directory/variables.tf
@@ -9,3 +9,23 @@ variable "slack_app_token" {
   type        = string
   sensitive   = true # Marks this as sensitive to prevent showing in logs
 }
+
+variable "azure_client_id" {
+  type      = string
+  sensitive = true
+}
+
+variable "azure_client_secret" {
+  type      = string
+  sensitive = true
+}
+
+variable "azure_tenant_id" {
+  type      = string
+  sensitive = true
+}
+
+variable "azure_subscription_id" {
+  type      = string
+  sensitive = true
+}
