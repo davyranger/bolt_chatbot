838021a228a060514258c041e5c06b8d592c3c81
Davy <42505476+davyranger@users.noreply.github.com>
2024-12-01 10:39:43 +0800

    Feature/002 (#166)
    * code refactoring
    
    * code refactoring
    
    * code refactoring
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code

---
+++ infra/modules/container/variables.tf
@@ -0,0 +1,29 @@
+variable "slack_app_token" {
+  type      = string
+  sensitive = true
+}
+
+variable "slack_bot_token" {
+  type      = string
+  sensitive = true
+}
+
+variable "azure_client_id" {
+  type      = string
+  sensitive = true
+}
+
+variable "azure_client_secret" {
+  type      = string
+  sensitive = true
+}
+
+variable "azure_tenant_id" {
+  type      = string
+  sensitive = true
+}
+
+variable "azure_subscription_id" {
+  type      = string
+  sensitive = true
+}
--- infra/parent_directory/container.tf
+++ infra/parent_directory/container.tf
@@ -1,3 +1,9 @@
 module "container" {
   source = "../modules/container"
+  azure_subscription_id = var.azure_subscription_id
+  azure_client_id = var.azure_client_id
+  azure_client_secret = var.azure_client_secret
+  azure_tenant_id = var.azure_tenant_id
+  slack_bot_token = var.slack_bot_token
+  slack_app_token = var.slack_app_token
 }
