8d677e60a763d4edf63b5e1a48d7fa5d367b6a9c
davy <davy.strain@hotmail.com>
2024-12-01 16:52:18 +0800

    adding in variables to container group for testing

--- infra/modules/container/outputs.tf
+++ infra/modules/container/outputs.tf
@@ -9,9 +9,9 @@ output "rg_name" {
   value = azurerm_resource_group.rg.name
 }
 
-output "uai_id" {
-  value = azurerm_user_assigned_identity.managed_identity.id
-}
+# output "uai_id" {
+#   value = azurerm_user_assigned_identity.managed_identity.id
+# }
 
 output "cg_id" {
   value = azurerm_container_registry.example.id
