b0f3066939103709daba3f8397c0311966c439c1
davy <davy.strain@hotmail.com>
2024-12-01 15:54:13 +0800

    adding in variables to container group for testing

--- infra/modules/container/outputs.tf
+++ infra/modules/container/outputs.tf
@@ -3,7 +3,7 @@ output "rg_id" {
 }
 
 output "uai_id" {
-  value = azurerm_user_assigned_identity.managed_identity.principal_id
+  value = azurerm_user_assigned_identity.managed_identity.name
 }
 
 output "cg_id" {
