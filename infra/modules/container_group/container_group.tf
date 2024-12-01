051a03d144d26738f343411d9db4a143b4c299a7
davy <davy.strain@hotmail.com>
2024-12-01 16:40:15 +0800

    adding in variables to container group for testing

--- infra/modules/container_group/container_group.tf
+++ infra/modules/container_group/container_group.tf
@@ -12,7 +12,7 @@ resource "azurerm_container_group" "example" {
 
   identity {
     type         = "UserAssigned"
-    identity_ids = "/subscriptions/${azure_subscription_id}/resourceGroups/slack-bot-rg/providers/Microsoft.ManagedIdentity/userAssignedIdentities/slackbot-identity"
+    identity_ids = "/subscriptions/${var.azure_subscription_id}/resourceGroups/slack-bot-rg/providers/Microsoft.ManagedIdentity/userAssignedIdentities/slackbot-identity"
   }
 
   container {
