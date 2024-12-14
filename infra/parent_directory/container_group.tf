56b5a84492367601ddf458d32de652db75dce989
Davy <42505476+davyranger@users.noreply.github.com>
2024-12-14 13:49:37 +0800

    Feature/002 (#191)
    * code refactoring
    
    * code refactoring
    
    * code refactoring
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * refactoring code
    
    * adding in variables to container group for testing
    
    * adding in variables to container group for testing
    
    * adding in variables to container group for testing
    
    * adding in variables to container group for testing
    
    * adding in variables to container group for testing
    
    * adding in variables to container group for testing
    
    * adding in variables to container group for testing
    
    * adding in variables to container group for testing
    
    * adding in variables to container group for testing
    
    * adding in variables to container group for testing
    
    * adding in variables to container group for testing
    
    * adding in variables to container group for testing
    
    * adding in variables to container group for testing
    
    * adding in variables to container group for testing
    
    * adding in variables to container group for testing
    
    * adding in variables to container group for testing
    
    * adding in variables to container group for testing
    
    * adding in variables to container group for testing
    
    * adding in variables to container group for testing
    
    * adding in variables to container group for testing
    
    * adding in variables to container group for testing
    
    * adding in variables to container group for testing
    
    * adding in variables to container group for testing
    
    * adding in variables to container group for testing
    
    * adding in variables to container group for testing
    
    * adding in variables to container group for testing
    
    * cleaning up code
    
    * commented out env
    
    * commented out env

--- infra/parent_directory/container_group.tf
+++ infra/parent_directory/container_group.tf
@@ -3,10 +3,10 @@ module "container_group" {
   resource_group_id       = module.container.rg_id
   resource_group_name     = module.container.rg_name
   resource_group_location = module.container.rg_location
-  container_registry    = module.container.cg_id
-  slack_bot_token       = var.slack_bot_token
-  slack_app_token       = var.slack_app_token
-  azure_subscription_id = var.azure_subscription_id
+  container_registry      = module.container.cg_id
+  slack_bot_token         = var.slack_bot_token
+  slack_app_token         = var.slack_app_token
+  azure_subscription_id   = var.azure_subscription_id
 
   depends_on = [
     module.container
